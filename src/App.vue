ca99c68c6067e501ed35b98765538002f46bf668
Ray Villalobos <ray@planetoftheweb.com>
2019-09-03 14:12:13 -0700

    Added Icons and bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div
+    id="app"
+    class="container"
+  >
+    <p>
+      <font-awesome-icon icon="trash" /> ipsum dolor, sit amet consectetur adipisicing elit. Distinctio eligendi harum possimus similique alias placeat odit excepturi ab magnam adipisci commodi id ipsum aliquam maiores nesciunt dolore a, quisquam facere!</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 export default {
   name: "app",
   components: {
-    HelloWorld
+    FontAwesomeIcon
   }
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "node_modules/bootstrap/scss/bootstrap";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faPlay, faPause, faTrash} from "@fortawesome/free-solid-svg-icons";
+library.add(faPlay, faPause, faTrash);
 
 Vue.config.productionTip = false;
 
