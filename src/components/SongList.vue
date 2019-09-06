1ce8faa27ffc0938c847ef63dfabb54433e522f4
Ray Villalobos <ray@planetoftheweb.com>
2019-09-06 14:28:31 -0700

    created a songlist component

---
+++ src/components/SongList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div
+      class="songlist list-group"
+      v-for="(song, index) in songs"
+      :key="index"
+    >
+      <div class="song list-group-item list-group-item-action d-flex">
+        <font-awesome-icon
+          class="trash mr-3 align-self-center"
+          icon="trash"
+        />
+        <div
+          class="thumbnail"
+          :style="{backgroundImage: 'url('+ song.thumb +')'}"
+        >
+        </div>
+        <div class="song-info flex-fill px-2 align-self-center">
+          <h4 class="song-info-name mb-0">{{song.name}}
+            <small class="duration">{{song.duration}}</small></h4>
+          <p class="song-info-creator mb-0">{{song.created_by}}</p>
+        </div>
+        <div class="now-playing d-flex align-items-center">
+          <font-awesome-icon
+            class="play"
+            icon="play"
+          />
+          <font-awesome-icon
+            class="pause"
+            icon="pause"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: "song-list",
+  props: ["songs"],
+  components: {
+    FontAwesomeIcon
+  }
+};
+</script>
